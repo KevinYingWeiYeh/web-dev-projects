267d2b1804f5baefa5f5f84c8a9631abeaab8219
Kevin Yeh <revinyeh@gmail.com>
2024-03-28 11:04:15 -0700

    add all social connection button with actural links

---
+++ personal-portfolio/src/components/Experience.js
@@ -0,0 +1,10 @@
+import { Container, Row, Col } from "react-bootstrap";
+import "animate.css";
+
+export const Experience = () => {
+  return (
+    <section className="banner" id="home">
+      <Container></Container>
+    </section>
+  );
+};
